cfda37640aa87900f8f3c9fac50afa64df4d294b
John Fischelli <john.fischelli@gmail.com>
2020-09-24 09:30:16 -0500

    add prettier

--- src/components/QueueSelector/QueueSelector.jsx
+++ src/components/QueueSelector/QueueSelector.jsx
@@ -1,53 +1,49 @@
-import React from "react";
-import "./styles.css";
-import * as Flex from '@twilio/flex-ui';
-import { connect } from 'react-redux';
+import React from 'react'
+import { connect } from 'react-redux'
+import * as Flex from '@twilio/flex-ui'
+
+import './styles.css'
 
 class QueueSelector extends React.Component {
+  toggleQueueFilterView = () => {
+    Flex.Actions.invokeAction('SetComponentState', {
+      name: 'QueueFilter',
+      state: { isHidden: !this.props.isHidden },
+    })
+  }
 
-    toggleQueueFilterView = () => {
-        
-        Flex.Actions.invokeAction('SetComponentState', {
-            name: 'QueueFilter',
-            state: { isHidden: !this.props.isHidden }
-        });
+  componentDidMount() {
+    Flex.Actions.invokeAction('SetComponentState', {
+      name: 'QueueFilter',
+      state: { isHidden: typeof this.props.isHidden === 'undefined' ? true : this.props.isHidden },
+    })
 
+    if (this.props.selectedValues) {
+      Flex.QueuesStats.setFilter((queue) => this.props.selectedValues.includes(queue.friendly_name))
     }
+  }
 
-    componentDidMount() {
-        Flex.Actions.invokeAction('SetComponentState', {
-            name: 'QueueFilter',
-            state: { isHidden: (typeof this.props.isHidden === "undefined") ? true : this.props.isHidden  }
-        });
-
-        if (this.props.selectedValues) {
-            Flex.QueuesStats.setFilter((queue) => this.props.selectedValues.includes(queue.friendly_name));
-        }
-    }   
-
-    render () {
-
-        return (
-            <div className="wrapper">
-                <div className="button" onClick={this.toggleQueueFilterView}>
-                    Filter <Flex.Icon icon="Filter"  />
-                </div>
-            </div>
-        )
-    } 
-       
-    
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="button" onClick={this.toggleQueueFilterView}>
+          Filter <Flex.Icon icon="Filter" />
+        </div>
+      </div>
+    )
+  }
 }
 
-const mapStateToProps = state => {
-    const componentViewStates = state.flex.view.componentViewStates;
-    const QueueFilterState = componentViewStates && componentViewStates.QueueFilter;
-    const isHidden = QueueFilterState && QueueFilterState.isHidden;
-    const selectedValues = state['flex'].worker.attributes['queues_view_filters'];
+const mapStateToProps = (state) => {
+  const componentViewStates = state.flex.view.componentViewStates
+  const QueueFilterState = componentViewStates && componentViewStates.QueueFilter
+  const isHidden = QueueFilterState && QueueFilterState.isHidden
+  const selectedValues = state['flex'].worker.attributes['queues_view_filters']
 
-    return {
-        isHidden,selectedValues
-    }
-};
+  return {
+    isHidden,
+    selectedValues,
+  }
+}
 
-export default connect(mapStateToProps)(QueueSelector);
+export default connect(mapStateToProps)(QueueSelector)
